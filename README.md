ab1e597973803ab734d782e1c30c6190f86a5acf
Mnw2212 <mayuresh2212@gmail.com>
2014-03-28 19:36:24 +0530

    Added Shell Sort

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ Algorithms
 ==========
 
 Algorithms
+
+Sorting:
+	1. Insertion Sort
+	2. Bubble Sort
+	3. Shell Sort	-reimplement
+
+
+Searching:
+	1. Linear Search
+	2. Binary Search
---
+++ Sorting/ShellSort.cpp
@@ -0,0 +1,35 @@
+/*
+ * 
+ * Shell Sort
+ * 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int Shell(int arr[],int size);
+void Swap(int a,int b);
+
+int main(void)
+{
+	int array[6]={6,2,4,1,3,5};
+	
+	Shell(array,6);
+	
+	return 0;
+}
+
+int Shell(int arr[],int size)
+{
+	int h=1;
+
+ 
+void Swap(int a,int b)
+{
+	int temp;
+	temp = a;
+	a=b;
+	b = temp;
+}
+
