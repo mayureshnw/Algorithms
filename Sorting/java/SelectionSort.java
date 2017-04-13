aa5b654a2e042e854d049fcd46a1375108a43c2d
Mayuresh Waykole <mayuresh_waykole@mckinsey.com>
2017-04-13 13:50:10 +0530

    Selection Sort

---
+++ Sorting/java/SelectionSort.java
@@ -0,0 +1,41 @@
+public class SelectionSort {
+
+  public static void log(int[] arr) {
+    for(int i=0;i<arr.length;i++) {
+      System.out.print(arr[i]+ " ");
+    }
+    System.out.println(" ");
+  }
+
+  public static int min(int[] arr, int start) {
+    int min = 2147483647;
+    int index = 0;
+    for(int i=start; i<arr.length; i++) {
+      if(arr[i] < min) {
+        min = arr[i];
+        index = i;
+      }
+    }
+    return index;
+  }
+
+  public static int[] sort(int[] arr) {
+    int arrayLength = arr.length;
+    int minIndex = 0;
+    int temp;
+    for(int i=0;i<arrayLength;i++) {
+      minIndex = min(arr, i);
+      System.out.println("min index: "+minIndex);
+      temp = arr[minIndex];
+      arr[minIndex] = arr[i];
+      arr[i] = temp;
+    }
+
+    return arr;
+  }
+
+  public static void main(String[] args) {
+    int[] arr = {4,7,1,3,9,67,12,45,87,65,77,20};
+    log(sort(arr));
+  }
+}
